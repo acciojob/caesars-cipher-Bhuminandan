cc3e5695e2c5663f1f485d1c9dc19edecd8b129d
Bhuminandan <134225833+Bhuminandan@users.noreply.github.com>
2023-07-08 15:17:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -32,20 +32,18 @@ const lookup = {
 };
 
 function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
+  var codeArr = encodedStr.split("");  // String to Array
+  var decodedArr = []; // Your Result goes here
+  // Only change code below this line
 
-	for (let i = 0; i < encodedStr.length; i++) {
-		if(encodedStr[i] != ' '){
-			decodedArr[i] = lookup.encodedStr[i];
-		}
-	}
-	
-  return decodedArr; //return decodedArr
+  decodedArr = codeArr.map(function(letter) {
+    if(lookup.hasOwnProperty(letter)) {
+      letter = lookup[letter];
+    }
+    return letter;
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-console.log(rot13("SERR YBIR? NPPVBWBO"));
 
 // Do not change this line
 window.rot13 = rot13;
